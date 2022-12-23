c01a841376d8217e50ef06aeacc8513cc790182d
Vqlcano <vqlcano@gmail.com>
2022-12-23 16:57:32 -0500

    added dark mode toogle and changed image file namng conventions to cryptography to allow for multiple uses of commands at once
    Co-authored-by: skearya <skearya@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# This example requires the 'members' and 'message_content' privileged intents to function.
 # TODO: implement tests, blacktea like game, images
 
 import discord
@@ -7,6 +6,10 @@ import random
 import config
 import json
 from PIL import Image, ImageDraw, ImageFont
+import os
+from os.path import exists
+import string
+
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 
@@ -16,7 +19,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix = '-', description = description, intents = intents)
+bot = commands.Bot(command_prefix = '-', description = description, intents = intents, activity = discord.Activity(type = discord.ActivityType.watching, name = "-help"))
 
 @bot.event
 async def on_ready():
@@ -29,6 +32,7 @@ with open("hiragana.json", encoding = "utf8") as hiraganaJSON:
     hiraganaJSON = json.loads(hiraganaJSON.read())
 for i in hiraganaJSON:
     hiraganaData.append([i["kana"], i["romaji"]])
+
 basicHiragana = []
 
 katakanaData = []
@@ -37,71 +41,85 @@ with open("katakana.json", encoding = "utf8") as katakanaJSON:
 for i in katakanaJSON:
     katakanaData.append([i["kana"], i["romaji"]])
 
+darkMode = False
+
 @bot.command()
-async def h(ctx): #Shortform Hiragana Command
-    await hiragana(ctx)
-@bot.command()
+async def darkmode(ctx):
+    global darkMode
+    embed = discord.Embed()
+    
+    if darkMode:
+        embed.color = 0xFFFFFF
+        embed.title = "Dark Mode Turned Off"
+        embed.set_footer(text = f"Light Mode requested by {ctx.author.display_name}", icon_url = ctx.author.avatar.url)
+    else:
+        embed.color = 0x000000
+        embed.title = "Dark Mode Turned On"
+        embed.set_footer(text = f"Dark Mode requested by {ctx.author.display_name}", icon_url = ctx.author.avatar.url)
+
+    darkMode = not darkMode
+
+    await ctx.send(embed = embed)
+    
+
+async def kana(ctx, type: str):
+    if type != "hiragana" and type != "katakana":
+        return
+
+    check = None
+    randKana = None
+    if type == "hiragana":
+        randKana = hiraganaData[random.randint(0, len(hiraganaData))]
+        check = "Hiragana"
+    elif type == "katakana":
+        randKana = katakanaData[random.randint(0, len(katakanaData))]
+        check = "Katakana"
+
+    ttf = ImageFont.truetype("ARIALUNI.TTF", 256)
+    message = str(randKana[0])
+    width = 512
+    height = 512
+
+    img = Image.new("RGB", (width, height), color = "black" if darkMode else "white")
+    imgDraw = ImageDraw.Draw(img)
+    textWidth, textHeight = imgDraw.textsize(message, font = ttf)
+    xText = (width - textWidth) / 2
+    yText = (height - textHeight) / 2
+
+    imgDraw.text((xText, yText), message, fill = "white" if darkMode else "black", font = ttf)
+
+    digits = 24
+    def generateCrypt(d: int):
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k = d))
+        
+    fileName = generateCrypt(digits)
+    while exists(f"{fileName}.png"):
+        fileName = generateCrypt(digits)
+
+    img.save(f"{fileName}.png")
+
+    embed = discord.Embed()
+    embed.title = f"**{check}**"
+    embed.add_field(name = "Character", value = f"**{randKana[0]}**", inline = True)
+    embed.add_field(name = "Romaji", value = f"||{randKana[1]}||", inline = True)
+    embed.color = 0x61D67E
+    embed.set_footer(text = f"{check} requested by {ctx.author.display_name}", icon_url = ctx.author.avatar.url)
+
+    file = discord.File(f"{fileName}.png", filename = f"{fileName}.png")
+    embed.set_image(url = f"attachment://{fileName}.png")
+
+    await ctx.send(file = file, embed = embed)
+
+    os.remove(f"{fileName}.png")
+
+
+@bot.command(aliases=["h"])
 async def hiragana(ctx):
-    randHiragana = hiraganaData[random.randint(0, len(hiraganaData))]
-    ttf = ImageFont.truetype("ARIALUNI.TTF", 256)
-    message = str(randHiragana[0])
-    width = 512
-    height = 512
+    await kana(ctx, "hiragana")
 
-    img = Image.new("RGB", (width, height), color = "white")
-    imgDraw = ImageDraw.Draw(img)
-    textWidth, textHeight = imgDraw.textsize(message, font = ttf)
-    xText = (width - textWidth) / 2
-    yText = (height - textHeight) / 2
-
-    imgDraw.text((xText, yText), message, fill = "black", font = ttf)
-
-    img.save("hiragana.png")
-
-    embed = discord.Embed()
-    embed.title = "**Hiragana**"
-    embed.add_field(name = "Character", value=f"**{randHiragana[0]}**", inline = True)
-    embed.add_field(name = "Romaji", value=f"||{randHiragana[1]}||", inline = True)
-    embed.color = 0x61D67E
-    embed.set_footer(text = f"Hiragana requested by {ctx.author.display_name}", icon_url=ctx.author.avatar.url)
-
-    file = discord.File("hiragana.png", filename = "hiragana.png")
-    embed.set_image(url = "attachment://hiragana.png")
-
-    await ctx.send(file = file, embed = embed)
-
-@bot.command()
-async def k(ctx): #Shortform Katakana Command
-    await katakana(ctx)
-@bot.command()
+@bot.command(aliases=["k"])
 async def katakana(ctx):
-    randKatakana = katakanaData[random.randint(0, len(katakanaData))]
-    ttf = ImageFont.truetype("ARIALUNI.TTF", 256)
-    message = str(randKatakana[0])
-    width = 512
-    height = 512
-
-    img = Image.new("RGB", (width, height), color = "white")
-    imgDraw = ImageDraw.Draw(img)
-    textWidth, textHeight = imgDraw.textsize(message, font = ttf)
-    xText = (width - textWidth) / 2
-    yText = (height - textHeight) / 2
-
-    imgDraw.text((xText, yText), message, fill = "black", font = ttf)
-
-    img.save("katakana.png")
-
-    embed = discord.Embed()
-    embed.title = "**Katakana**"
-    embed.add_field(name = "Character", value=f"**{randKatakana[0]}**", inline = True)
-    embed.add_field(name = "Romaji", value=f"||{randKatakana[1]}||", inline = True)
-    embed.color = 0x61D67E
-    embed.set_footer(text = f"Katakana requested by {ctx.author.display_name}", icon_url=ctx.author.avatar.url)
-
-    file = discord.File("katakana.png", filename = "katakana.png")
-    embed.set_image(url = "attachment://katakana.png")
-
-    await ctx.send(file = file, embed = embed)
+    await kana(ctx, "katakana")
 
 bot.run(config.token)
 
